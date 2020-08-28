7b2a80ce84af3756203e55509fd54becf3356f72
Brais Moure <mouredev@gmail.com>
2020-08-28 18:09:12 +0200

    ** Nerdéame Día 1: - Creación del proyecto.

---
+++ nerdeame-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nerdeame-ios
+//
+//  Created by Brais Moure on 28/08/2020.
+//  Copyright © 2020 MoureDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
